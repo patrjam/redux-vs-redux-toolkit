900ed12c5cd3a805a1714bcacafe778f7538d770
Patricia Jamriskova <patricia.jamriskova01@dixonscarphone.com>
2022-03-10 22:50:44 +0100

    works, but not correct use

--- app-with-redux/src/redux/action-creator/index.js
+++ app-with-redux/src/redux/action-creator/index.js
@@ -1,25 +1,62 @@
+import {
+  fetchDepositError,
+  fetchDepositRequest,
+} from "../reducers/accountReducer.js";
+import { fetchDepositSuccess } from "../reducers/accountReducer.js";
+import {
+  fetchWithdrawRequest,
+  fetchWithdrawSuccess,
+  fetchWithdrawError,
+  fetchDepositInterestRateRequest,
+  fetchDepositInterestRateSuccess,
+  fetchDepositInterestRateError,
+} from "../reducers/accountReducer.js";
+
 export const depositMoney = (amount) => {
   return (dispatch) => {
-    dispatch({
-      type: "deposit",
-      payload: amount,
+    dispatch(fetchDepositRequest());
+    new Promise((resolve, reject) => {
+      const randomError = Math.floor(Math.random() * 4); 
+      randomError !== 0 // 25% chance of failure
+        ? resolve(
+            setTimeout(() => {
+              console.log("Adding money...");
+              dispatch(fetchDepositSuccess(amount));
+            }, 2000)
+          )
+        : reject(dispatch(fetchDepositError("Your money was stolen.")));
     });
   };
 };
 
 export const withdrawMoney = (amount) => {
   return (dispatch) => {
-    dispatch({
-      type: "withdraw",
-      payload: amount,
+    dispatch(fetchWithdrawRequest());
+    new Promise((resolve, reject) => {
+      const randomError = Math.floor(Math.random() * 4); 
+      randomError !== 0 // 25% chance of failure
+        ? resolve(
+            setTimeout(() => {
+              console.log("Removing money...");
+              dispatch(fetchWithdrawSuccess(amount));
+            }, 2000)
+          )
+        : reject(dispatch(fetchWithdrawError("Your money was stolen.")));
     });
   };
 };
 
 export const depositInterestRate = () => {
   return (dispatch) => {
-    dispatch({
-      type: "depositInterestRate"
-    })
-  }
-}
+    dispatch(fetchDepositInterestRateRequest());
+    const randomError = Math.floor(Math.random() * 3); 
+
+    console.log(randomError);
+
+    randomError === 2 //33% change of failure
+      ? dispatch(
+          fetchDepositInterestRateError("Your 2% interest rate of money was stolen.")
+        )
+      : dispatch(fetchDepositInterestRateSuccess());
+  };
+};
--- app-with-redux/src/redux/reducers/index.js
+++ app-with-redux/src/redux/reducers/index.js
@@ -1,8 +1,18 @@
-import { combineReducers } from "redux";
-import accountReducer from "./accountReducer";
+// import { combineReducers } from "redux";
+// import {accountReducer1} from "./accountReducer";
 
-const reducers = combineReducers({
-  account: accountReducer,
-});
+// const reducers = combineReducers({
+//   account: accountReducer1,
+// });
 
-export default reducers;
+// export default reducers;
+
+
+// const services = {
+//   // depositMoney: depositMoneyService,
+//   // withdrawMoney: withdrawMoneyService,
+//   // depositInterestRate: depositInterestRateService
+//   account: accountServices
+// }
+
+// export default services
