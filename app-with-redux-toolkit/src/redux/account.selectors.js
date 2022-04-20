c098d4e733d1df486068b15c4f25ca96adff9adf
Patricia Jamriskova <patricia.jamriskova01@dixonscarphone.com>
2022-04-20 13:09:58 +0200

    add shitty code

---
+++ app-with-redux-toolkit/src/redux/account.selectors.js
@@ -0,0 +1,4 @@
+export const getState = (state) => state.account.account;
+export const getAmount = (state) => state.account.account.amount;
+export const getLoading = (state) => state.account.account.loading;
+export const getError = (state) => state.account.account.error;
