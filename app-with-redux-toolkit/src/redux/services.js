abbe571548b6a0e828317bf6392347a669fcaa60
Patricia Jamriskova <patricia.jamriskova01@dixonscarphone.com>
2022-05-02 15:36:00 +0200

    add reworked services solution with dependency injection

--- app-with-redux-toolkit/src/redux/services.js
+++ app-with-redux-toolkit/src/redux/services.js
@@ -1,23 +1,22 @@
 let accountBalance = 0;
 
 export const changeAccountState = async (
-    deltaAmount,
-    interestRateCalculation = false
+  deltaAmount,
+  interestRateCalculation = false
 ) => {
-    // todo what about : const randomError = Math.random()<=0.25 ?
-    const randomError = Math.floor(Math.random() * 4);
-    return await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            randomError !== 0 // 25% chance of failure
-                ? resolve((accountBalance = Math.round(accountBalance + deltaAmount)))
-                : reject(
-                    interestRateCalculation ? accountBalance : (accountBalance = 0),
-                    new Error("Sorry, money stolen!")
-                );
-        }, 2000);
-    });
+  const randomError = Math.random() <= 0.75;
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      randomError
+        ? resolve((accountBalance = Math.round(accountBalance + deltaAmount)))
+        : reject(
+            interestRateCalculation ? accountBalance : (accountBalance = 0),
+            new Error("Sorry, money stolen!")
+          );
+    }, 2000);
+  });
 };
 
 export const actualBalance = () => {
-    return accountBalance;
+  return accountBalance;
 };
